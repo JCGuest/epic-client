b20f4ed560f46eb646d7543a670a83b02267e757
JCGuest <jcguest90@gmail.com>
2022-01-22 21:51:35 -0600

    vercel

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { ethers } from "ethers";
 import myEpicNft from "./utils/MyEpicNFT.json"
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'johnguestdev';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0xe556b0ad40B7A725dbc73Df2e1FF02384B136873";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
@@ -32,6 +33,7 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account)
+      setupEventListener()
     } else {
       console.log("No authorized account found")
     }
@@ -51,6 +53,7 @@ const connectWallet = async () => {
     */
     console.log("Connected", accounts[0]);
     setCurrentAccount(accounts[0]); 
+    setupEventListener() 
   } catch (error) {
     console.log(error)
   }
@@ -63,9 +66,32 @@ const connectWallet = async () => {
   </button>
 );
 
-const askContractToMintNft = async () => {
-  const CONTRACT_ADDRESS = "0x495225d789B390b012079623DC0eD015D3619228";
+// Setup our listener.
+const setupEventListener = async () => {
+  try {
+    const { ethereum } = window;
 
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+      connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+        console.log(from, tokenId.toNumber())
+        alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+      });
+
+      console.log("Setup event listener!")
+
+    } else {
+      console.log("Ethereum object doesn't exist!");
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const askContractToMintNft = async () => {
   try {
     const { ethereum } = window;
 
@@ -78,6 +104,8 @@ const askContractToMintNft = async () => {
       let nftTxn = await connectedContract.makeAnEpicNFT();
 
       console.log("Mining...please wait.")
+      alert("Mining...please wait.")
+      
       await nftTxn.wait();
       
       console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
@@ -90,9 +118,11 @@ const askContractToMintNft = async () => {
   }
 }
 
-  /*
-  * This runs our function when the page loads.
-  */
+const openCollection = () => {
+  window.open('https://testnets.opensea.io/collection/3wordnft-t6qy11gqwz', '_blank');
+
+}
+
   useEffect(() => {
       checkIfWalletIsConnected();
   }, [])
@@ -101,15 +131,22 @@ const askContractToMintNft = async () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">My NFT Collection</p>
+          <p className="header gradient-text">NFT Marketplace</p>
           <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
+
+            <p><small>Connect your wallet and switch to Rinkeby network.</small><br></br> 
+            <small>Each token is randomly generated from a selection of colors and sci-fi related words.</small></p>
+
+            {/* <a href="https://testnets.opensea.io/collection/3wordnft-t6qy11gqwz/" target="_blank">Here</a> is a link to the collection on OpenSea. */}
+            
           </p>
+            <button onClick={openCollection} className="cta-button connect-wallet-button">view collection</button>
+            <>                                                                                                 </>
           {currentAccount === "" ? renderNotConnectedContainer():
             (
               /** Add askContractToMintNft Action for the onClick event **/
               <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
-              Mint NFT
+              mint new NFT
               </button>
             )
           }
@@ -121,7 +158,7 @@ const askContractToMintNft = async () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`@${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
